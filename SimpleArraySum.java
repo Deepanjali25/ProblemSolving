5677cc1a6286f3b8b1a141b0b21206ff598253e5
Deepanjali <38421590+Deepanjali25@users.noreply.github.com>
2019-08-09 19:51:14 +0530

    Add files via upload

--- SimpleArraySum.java
+++ SimpleArraySum.java
@@ -1,31 +1,17 @@
 package Hackerrank.ProblemSolving;
 import java.io.*;
-import java.math.*;
-import java.text.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
 import java.util.regex.*;
 public class SimpleArraySum {
-    /*
-     * Complete the simpleArraySum function below.
-     */
-    static int simpleArraySum(int[] ar) {
-        /*
-         * Write your code here.
-         */
-    }
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-        int arCount = Integer.parseInt(scanner.nextLine().trim());
-        int[] ar = new int[arCount];
-        String[] arItems = scanner.nextLine().split(" ");
-        for (int arItr = 0; arItr < arCount; arItr++) {
-            int arItem = Integer.parseInt(arItems[arItr].trim());
-            ar[arItr] = arItem;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int sum = 0;
+        for(int arr_i=0; arr_i < n; arr_i++){
+            sum += in.nextInt();
         }
-        int result = simpleArraySum(ar);
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-        bufferedWriter.close();
+        System.out.println(sum);
     }
 }
